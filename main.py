40eed352d9292595813f5bff95779af7ac41d0bc
LukeT23 <61421060+LukeT23@users.noreply.github.com>
2021-03-04 14:39:27 -0800

    tested increasing character xp with enemy xp

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import enum
 
 import NPC
 import Characters
+import Enemies
 
 bob = NPC.NPC("Bob", "DUDE", 5, 4, 50)
 
@@ -19,17 +20,33 @@ badWarrior = Characters.Character("Warrior", "A Warrior", 30, 50, 50, Characters
 
 badWizard = Characters.Character("Wizard", "A Wizard", 10, 20, 50, Characters.Occupations.Cop, "True", 50)
 
+badGuy = Enemies.Enemy("Bad Guy", "Not good", 30, 20, 30, 2, False, 0, 50)
+
 print("Warrior:", warrior.get_has_magic(), "POINTS: ", warrior.get_magic())
 print("Wizard:", wizard.get_has_magic(), "POINTS: ", wizard.get_magic())
 print("badWarrior:", badWarrior.get_has_magic(), "POINTS: ", badWarrior.get_magic())
 print("badWizard:", badWizard.get_has_magic(), "POINTS: ", badWizard.get_magic())
+print()
 
 print("Warrior Level: ", warrior.get_level())
 warrior.increase_experience(25)
+
 print("Warrior XP: ", warrior.get_experience())
 print("Warrior XP_REQ", warrior.get_experience_req())
+print()
 print("INCREASING LEVEL")
-warrior.increase_experience(150)
+print()
+warrior.increase_experience(85)
+
+print("Warrior Level: ", warrior.get_level())
+print("Warrior XP: ", warrior.get_experience())
+print("Warrior XP_REQ", warrior.get_experience_req())
+print()
+
+print("Increasing with Enemy XP")
+print()
+warrior.increase_experience(badGuy.get_experience())
+
 print("Warrior Level: ", warrior.get_level())
 print("Warrior XP: ", warrior.get_experience())
 print("Warrior XP_REQ", warrior.get_experience_req())
