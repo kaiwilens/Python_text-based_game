6558660a67cf34731d3bf2dfeb756f90f12a3b12
Ghraam <terbom@protonmail.com>
2021-03-29 16:50:48 -0400

    made requested changes

--- Battle_Sequence.py
+++ Battle_Sequence.py
@@ -1,4 +1,4 @@
-def do_battle(self, enemies):
+def do_battle(character, enemies):
     """
     Used to battle enemies.
 
@@ -11,7 +11,7 @@ def do_battle(self, enemies):
         while True:
             while True:
                 try:
-                    print("Your health: ", self.get_health())
+                    print("Your health: ", character.get_health())
                     print("Enemy health: ", enemyHealth)
                     print(
                         "(1) heal\n"
@@ -27,23 +27,23 @@ def do_battle(self, enemies):
                     print("Error: Input must be a number.")
             # Do stuff based on option
             if option == 1:
-                self.heal_character(1)
+                character.heal_character(1)
             elif option == 2:
-                if self.get_has_magic():
+                if character.get_has_magic():
                     # How does magic work?
                     print("Magic!")
                 else:
                     print("Error: No magic.")
             elif option == 3:
-                enemy.set_health(enemyHealth - self.get_strength())
+                enemy.set_health(enemyHealth - character.get_damage())
                 print("<<<!!!>>>")
             # Check if enemy is still alive
             enemyHealth = enemy.get_health()
             if not enemyHealth:
                 break
             # Enemy attack YOU
-            self.reduce_health(enemy.get_strength())
-            if not self.get_health():
+            character.reduce_health(enemy.get_damage())
+            if not character.get_health():
                 print("You died!")
                 return
         print("DEAD!")
