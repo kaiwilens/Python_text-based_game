706f91a76e2083b60faf483ddf4bf4bac48598a9
Ghraam <terbom@protonmail.com>
2021-04-06 03:51:36 -0400

    finished title screen

--- Title_Screen.py
+++ Title_Screen.py
@@ -1,18 +1,35 @@
 import os
 import math
 
+banner = [
+    "  ______                                      __   ____              ___ __       ",
+    " /_  __/__  ____ ___  ____  ____  _________ _/ /  / __ \__  ______ _/ (_) /___  __",
+    "  / / / _ \/ __ `__ \/ __ \/ __ \/ ___/ __ `/ /  / / / / / / / __ `/ / / __/ / / /",
+    " / / /  __/ / / / / / /_/ / /_/ / /  / /_/ / /  / /_/ / /_/ / /_/ / / / /_/ /_/ / ",
+    "/_/  \___/_/ /_/ /_/ .___/\____/_/   \__,_/_/  /_____/\__,_/\__,_/_/_/\__/\__, /  ",
+    "                  /_/                                                    /____/   ",
+]
 
 def title_screen():
-    rows = int(os.popen("tput lines").read())
-    cols = int(os.popen("tput cols").read())
-    print("-" * cols)
-    print(("|" + " " * (cols - 2) + "|") * int((rows - 3) / 2 - 3))
-    print("|" + " " * math.floor((cols - 84) / 2) + "  ______                                      __   ____              ___ __       " + " " * math.ceil((cols - 84) / 2) + "|")
-    print("|" + " " * math.floor((cols - 84) / 2) + " /_  __/__  ____ ___  ____  ____  _________ _/ /  / __ \__  ______ _/ (_) /___  __" + " " * math.ceil((cols - 84) / 2) + "|")
-    print("|" + " " * math.floor((cols - 84) / 2) + "  / / / _ \/ __ `__ \/ __ \/ __ \/ ___/ __ `/ /  / / / / / / / __ `/ / / __/ / / /" + " " * math.ceil((cols - 84) / 2) + "|")
-    print("|" + " " * math.floor((cols - 84) / 2) + " / / /  __/ / / / / / /_/ / /_/ / /  / /_/ / /  / /_/ / /_/ / /_/ / / / /_/ /_/ / " + " " * math.ceil((cols - 84) / 2) + "|")
-    print("|" + " " * math.floor((cols - 84) / 2) + "/_/  \___/_/ /_/ /_/ .___/\____/_/   \__,_/_/  /_____/\__,_/\__,_/_/_/\__/\__, /  " + " " * math.ceil((cols - 84) / 2) + "|")
-    print("|" + " " * math.floor((cols - 84) / 2) + "                  /_/                                                    /____/   " + " " * math.ceil((cols - 84) / 2) + "|")
-    print(("|" + " " * (cols - 2) + "|") * int((rows - 3) / 2 - 3))
-    print("-" * cols)
-    input("Press Enter to continue")
+    badResponse = False
+    while True:
+        os.system("clear")
+        rows = int(os.popen("tput lines").read())
+        cols = int(os.popen("tput cols").read())
+        print("-" * cols)
+        print(("|" + " " * (cols - 2) + "|") * math.ceil((rows - math.ceil(len(banner) / 2)) / 2 - 3))
+        for line in banner:
+            print("|" + " " * math.floor((cols - len(line) - 2) / 2) + line + " " * math.ceil((cols - len(line) - 2) / 2) + "|")
+        print(("|" + " " * (cols - 2) + "|") * math.floor((rows - math.floor(len(banner) / 2)) / 2 - 3 - 2))
+        print("|1. Start Game" + " " * (cols - 15) + "|")
+        print("|2. Exit      " + " " * (cols - 15) + "|")
+        print("-" * cols)
+        try:
+            response = int(input("Invalid response! " if badResponse else ""))
+            if response == 1:
+                return
+            elif response == 2:
+                exit()
+        except ValueError:
+            pass
+        badResponse = True
