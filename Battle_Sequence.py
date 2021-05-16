cebe07dbb56772a1452fad289f466540e1ddf82d
LukeT23 <61421060+LukeT23@users.noreply.github.com>
2021-05-15 20:32:02 -0700

    removed unnecessary battle function

--- Battle_Sequence.py
+++ Battle_Sequence.py
@@ -9,68 +9,6 @@ class return_options(enum.Enum):
     defeat = 1
     retreat = 2
 
-
-def single_battle(characters, character, enemy):
-    """Fight a single enemy
-
-    This code is used for a single character to fight a single enemy.
-    The function ends once the enemy or the character is dead. If the character
-    dies they will be removed from the party list.
-
-    :param characters: List that contains the players party
-    :param character: The character that will be fighting
-    :param enemy: The enemy the character will fight
-    :return: breaks out of battle if all characters are dead
-    """
-    print(enemy.get_description())
-
-    # characters = list(characters)
-    while character.get_is_alive() and enemy.get_is_alive():
-        character.hub()
-        enemy.hub()
-
-        valid_input = False
-
-        while not valid_input:
-
-            print("1: Attack")
-            print("2: Retreat")
-            print("3: Inventory")
-
-            selection = input()
-
-            try:
-                int_selection = int(selection)
-
-                if int_selection == 1:
-                    enemy.reduce_health(character.get_damage())
-                    character.reduce_health(enemy.get_damage())
-                    valid_input = True
-
-                elif int_selection == 2:
-                    print("Retreat")
-
-                elif int_selection == 3:
-                    print("Inventory")
-
-                else:
-                    print("Invalid Selection")
-
-
-            except:
-                print("Invalid Selection")
-                print()
-
-    if not enemy.get_is_alive():
-        print(enemy.get_name(), " has been defeated")
-        enemy.set_is_alive(True)
-
-    else:
-        print(character.get_name(), "is dead")
-        characters.remove(character)
-
-
-
 def same_enemy_battle(characters, enemy, num_enemies, retreat_option = True):
     """Fight numerous enemies
 
